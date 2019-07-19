76c84171bb69a390eab939d77ab0d7ab651d4bc8
e_vtolar <vaclav_tolar@kb.cz>
2019-07-19 09:28:35 +0200

    New Dashboard Activity.

--- app/src/main/java/com/scodash/android/NewDashboardActivity.java
+++ app/src/main/java/com/scodash/android/NewDashboardActivity.java
@@ -1,5 +1,11 @@
 package com.scodash.android;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,5 +23,54 @@ public class NewDashboardActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        StepsPageAdapter stepsPageAdapter = new StepsPageAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(stepsPageAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private class StepsPageAdapter extends FragmentPagerAdapter {
+
+        public StepsPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Nullable
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new DashboardNameFragment();
+                case 1:
+                    return new DashboardItemsFragment();
+                case 2:
+                    return new DashboardAuthorFragment();
+            }
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.dashboard_name_tab);
+                case 1:
+                    return getResources().getText(R.string.dashboard_items_tab);
+                case 2:
+                    return getResources().getText(R.string.dashboard_author_tab);
+            }
+            return null;
+
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
