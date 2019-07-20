065b98dc77ef28a7d99ae0d1e15ecb3b8ac307e5
Vaclav Tolar <vaclav.tolar@gmail.com>
2019-07-20 21:49:52 +0200

    Items fragment.

--- app/src/main/java/com/scodash/android/DashboardItemsFragment.java
+++ app/src/main/java/com/scodash/android/DashboardItemsFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class DashboardItemsFragment extends Fragment {
@@ -14,7 +16,11 @@ public class DashboardItemsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard_items, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dashboard_items, container, false);
+        ListView itemsView = rootView.findViewById(R.id.items);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, NewDashboard.items);
+        itemsView.setAdapter(adapter);
+        return rootView;
     }
 
 }
---
+++ app/src/main/java/com/scodash/android/NewDashboard.java
@@ -0,0 +1,6 @@
+package com.scodash.android;
+
+public class NewDashboard {
+
+    public static String[] items = new String[] {"aaaa", "bbbb", "cccc"};
+}
