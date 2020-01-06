45532ef6eade144637123a250017692b6dce9914
Vaclav Tolar <vaclav.tolar@gmail.com>
2020-01-06 23:34:00 +0100

    GUI adjustments.

---
+++ app/src/main/java/com/scodash/android/activities/ScodashShareActionProvider.java
@@ -0,0 +1,16 @@
+package com.scodash.android.activities;
+
+import android.content.Context;
+
+import androidx.appcompat.widget.ShareActionProvider;
+
+public class ScodashShareActionProvider extends ShareActionProvider {
+    /**
+     * Creates a new instance.
+     *
+     * @param context Context for accessing resources.
+     */
+    public ScodashShareActionProvider(Context context) {
+        super(context);
+    }
+}
