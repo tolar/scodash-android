b2c38ab1f85ec9d3e713c5194bcf27cde3cc7740
Vaclav Tolar <vaclav.tolar@gmail.com>
2020-01-09 23:45:11 +0100

    No swipeable new dashboard wizard

---
+++ app/src/main/java/com/scodash/android/activities/NonSwipeableViewPager.java
@@ -0,0 +1,25 @@
+package com.scodash.android.activities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
